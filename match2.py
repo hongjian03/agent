a848b5ed5d7be5d045529efd1cd19d1f376ad179
hongjian03 <2692090126@qq.com>
2025-02-25 16:55:48 +0800

    Add files via upload

--- match2.py
+++ match2.py
@@ -155,10 +155,12 @@ def Consultant_matching(consultant_tags_file, merge_df):
         # 1. 国家标签匹配
         if '国家标签' in case and pd.notna(case['国家标签']):
             case_countries = set(re.split(r'[、,，]', case['国家标签']))
-            
+            st.write(f"案例国家: {case_countries}")
             # 获取顾问的各级别国家集合
             absolute_high_freq = set(re.split(r'[、,，]', consultant['绝对高频国家'])) if pd.notna(consultant['绝对高频国家']) else set()
             relative_high_freq = set(re.split(r'[、,，]', consultant['相对高频国家'])) if pd.notna(consultant['相对高频国家']) else set()
+            st.write(f"顾问绝对高频国家: {absolute_high_freq}")
+            st.write(f"顾问相对高频国家: {relative_high_freq}")
             # 检查是否完全包含目标国家
             if case_countries.issubset(absolute_high_freq):
                 tag_score_dict['绝对高频国家'] = tag_weights['绝对高频国家']
